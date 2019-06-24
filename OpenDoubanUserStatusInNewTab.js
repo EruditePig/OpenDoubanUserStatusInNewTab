a8ba3efa59188dd95b317c115ac485eadeda81f6
EruditePig <EruditePig@users.noreply.github.com>
2019-06-24 09:26:55 +0800

    delete log output
    delete log output

--- OpenDoubanUserStatusInNewTab.js
+++ OpenDoubanUserStatusInNewTab.js
@@ -2,7 +2,7 @@
 // @name            在豆瓣主页点击其他用户发表的某个状态，在新Tab页打开详细页面
 // @namespace       http://tampermonkey.net/
 // @description     Test
-// @version         0.0.2
+// @version         0.0.3
 // @author          EruditePig
 // @include         https://www.douban.com/*
 // ==/UserScript==]
@@ -13,7 +13,6 @@
 
 function OpenDoubanUserStatusInNewTab() 
 {
-    console.log("here");
     var douban_status_id = this.getAttributeNode("data-sid").value;
     var douban_user_id = this.getAttributeNode("data-uid").value;
     var url = "https://www.douban.com/people/" + douban_user_id + "/status/" + douban_status_id;
