a6d7899f85d1145e56185b60b3acd331016e8237
EruditePig <EruditePig@users.noreply.github.com>
2019-06-28 17:02:46 +0800

    改为按钮触发
    改为按钮触发

--- OpenDoubanUserStatusInNewTab.js
+++ OpenDoubanUserStatusInNewTab.js
@@ -2,7 +2,7 @@
 // @name            在豆瓣主页点击其他用户发表的某个状态，在新Tab页打开详细页面
 // @namespace       http://tampermonkey.net/
 // @description     Test
-// @version         0.0.4
+// @version         0.0.5
 // @author          EruditePig
 // @include         /^https://www\.douban\.com/(\?p=[0-9]+)*$/
 // ==/UserScript==]
@@ -13,16 +13,19 @@
 
 function OpenDoubanUserStatusInNewTab() 
 {
-    var douban_status_id = this.getAttributeNode("data-sid").value;
-    var douban_user_id = this.getAttributeNode("data-uid").value;
+    var douban_status_id = this.parentElement.getAttributeNode("data-sid").value;
+    var douban_user_id = this.parentElement.getAttributeNode("data-uid").value;
     var url = "https://www.douban.com/people/" + douban_user_id + "/status/" + douban_status_id;
     window.open(url, '_blank');
 };
-var elems = document.getElementsByClassName("status-item");
+var elems = document.getElementsByClassName("new-status");
 
 Array.from(elems).forEach((el) => {
     // Do stuff here
-    el.addEventListener("click", OpenDoubanUserStatusInNewTab);
+    var btn = document.createElement("BUTTON");   // Create a <button> element
+    btn.innerHTML = "NewTabOpen";                   // Insert text
+    btn.onclick = OpenDoubanUserStatusInNewTab;
+    el.appendChild(btn);
 });
 
 })();
