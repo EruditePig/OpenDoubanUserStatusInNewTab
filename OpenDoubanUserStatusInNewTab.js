cd1b141baeed1114f6ac0766fd70ca415daef396
EruditePig <EruditePig@users.noreply.github.com>
2019-07-02 08:30:33 +0800

    改变按钮的位置
    改变按钮的位置

--- OpenDoubanUserStatusInNewTab.js
+++ OpenDoubanUserStatusInNewTab.js
@@ -2,7 +2,7 @@
 // @name            在豆瓣主页点击其他用户发表的某个状态，在新Tab页打开详细页面
 // @namespace       http://tampermonkey.net/
 // @description     Test
-// @version         0.0.5
+// @version         0.0.6
 // @author          EruditePig
 // @include         /^https://www\.douban\.com/(\?p=[0-9]+)*$/
 // ==/UserScript==]
@@ -13,8 +13,8 @@
 
 function OpenDoubanUserStatusInNewTab() 
 {
-    var douban_status_id = this.parentElement.getAttributeNode("data-sid").value;
-    var douban_user_id = this.parentElement.getAttributeNode("data-uid").value;
+    var douban_status_id = this.parentElement.parentElement.parentElement.getAttributeNode("data-sid").value;
+    var douban_user_id = this.parentElement.parentElement.parentElement.getAttributeNode("data-uid").value;
     var url = "https://www.douban.com/people/" + douban_user_id + "/status/" + douban_status_id;
     window.open(url, '_blank');
 };
@@ -25,7 +25,10 @@ Array.from(elems).forEach((el) => {
     var btn = document.createElement("BUTTON");   // Create a <button> element
     btn.innerHTML = "NewTabOpen";                   // Insert text
     btn.onclick = OpenDoubanUserStatusInNewTab;
-    el.appendChild(btn);
+    btn.className = "erebor-ad";
+    var children = el.getElementsByClassName("hd");
+
+    children[0].appendChild(btn);
 });
 
 })();
